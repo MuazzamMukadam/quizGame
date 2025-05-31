b9109aaf338eac444245fd64c0e1410a6d6f4820
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-05-31 16:59:26 +0530

    k

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 
 
 
-
+const optionsEl = document.querySelectorAll('#options label');
 let qn = document.getElementById('qn');
+let qnval = 1;
+const Chkoptions = document.querySelectorAll('input[name="same"]');
+let isSelected = false;
+let doneInd = [];
 const questions = [
   {
     question: "What is the capital of India?",
@@ -26,12 +30,40 @@ const questions = [
     answer: "Cascading Style Sheets",
   },
 ];
-const ranInd = Math.floor(Math.random()*questions.length);
+function start(){
+
+ const ranInd = Math.floor(Math.random()*questions.length);
+     if(!doneInd.includes(ranInd)){
+doneInd.push(ranInd);
 let quesArea = document.querySelector('#question h3');
 quesArea.innerText = questions[ranInd].question;
+optionsEl.forEach((node,i)=>{
+node.querySelector('span').innerText = questions[ranInd].options[i];
+});
+}else{
+    start();
+}
+}
 
 function play() {
   document.getElementById("intro").style.display = "none";
   document.querySelector(".main").style.display = "block";
+  start();
+  
 }
-console.log(ranInd);
+function next(){
+for (const option of Chkoptions) {
+  if (option.checked) {
+    isSelected = true;
+    break;
+  }
+}
+if(isSelected){
+qn.innerText = ++qnval;
+isSelected = false;
+Chkoptions.forEach(option => option.checked = false);
+start();
+}else{
+    alert('Please select an option');
+}
+}
